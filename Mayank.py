4091d7ccbef40a14021411dbcb0ab03c9ba78940
GamerNishant <official007nishant@gmail.com>
2025-03-21 23:19:18 +0530

    Python Fundamentals

---
+++ Mayank.py
@@ -0,0 +1,28 @@
+# for start
+sum = 0
+for i in range(1,11):
+    sum += i
+
+print(sum)
+
+# while start
+sum = 0
+i = 1
+while i<11:
+    sum += i
+    i+=1
+
+print(sum)
+
+#list 
+arr = [1, 2, 3]
+i=0
+while i < len(arr):
+    print(arr[i])
+    i+=1
+
+
+dict  = {
+    "key" : "Value",
+    "Mayank" : "Management"
+}
